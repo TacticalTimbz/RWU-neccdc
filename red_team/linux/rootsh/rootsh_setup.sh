250af216bddf2458ca340be9172b88422c2c0259
TacticalTimbz <orlandoyeo@gmail.com>
2024-12-07 21:51:02 -0500

    adding ubuntu option for rootsh setup

--- red_team/linux/rootsh/rootsh_setup.sh
+++ red_team/linux/rootsh/rootsh_setup.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 source /etc/os-release
 if [ $ID="fedora" ]; then
-    dnf install -y rootsh
+  dnf install -y rootsh
+elif [ $ID="ubuntu"]; then
+  apt install -y rootsh
 else
-    echo false
+  echo "unable to install rootsh"
 fi
+
 if [ -f "/etc/default/useradd" ]; then
   cp -f useradd /etc/default/useradd
 else
   echo "could not find /etc/default/useradd"
 fi
+
+if [ -f "/etc/shells" ]; then
+  cp -f shells /etc/shells
+else  
+  echo "could not find /etc/shells
